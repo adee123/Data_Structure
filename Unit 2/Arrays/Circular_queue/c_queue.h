2a1ac7a4fc3882aa2ee0f33ba1d0424bb658d1d9
adee123 <87965571+adee123@users.noreply.github.com>
2021-12-31 18:35:00 +0530

    Add files via upload
    Circular Queue added

---
+++ Unit 2/Arrays/Circular_queue/c_queue.h
@@ -0,0 +1,23 @@
+#ifndef C_QUEUE_H
+#define C_QUEUE_H
+#include <stdbool.h>
+
+#define MAX_QUEUE_SIZE 100
+
+typedef int QueueEntry;
+
+typedef struct queue {
+    QueueEntry elements[MAX_QUEUE_SIZE];
+    int        front;
+    int        rear;
+    int        count;
+} Queue;
+
+void queue_init(Queue *queue);
+void queue_peek(Queue* queue, QueueEntry *entry);
+void queue_enqueue(Queue* queue, QueueEntry entry);
+void queue_dequeue(Queue* queue, QueueEntry *entry);
+bool queue_is_empty(Queue* queue);
+bool queue_is_full(Queue* queue);
+
+#endif
